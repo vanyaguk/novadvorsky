de29bf711f0458ace7f13174f5eb3c2d1d9a183b
vanyaguk <vanyaguk01@gmail.com>
2022-06-02 06:18:42 +0300

    ,

--- script.js
+++ script.js
@@ -51,3 +51,42 @@ function menushown() {
 
 
   
+$('form').submit(function(e){
+  e.preventDefault();
+  $.ajax({
+      url: 'https://docs.google.com/forms/d/e/1FAIpQLSdA8vhbKOez5PW_0NxCfswJjEl5mPWjHcV-Vf8yuvgmSYa_vg/formResponse',
+      type: 'post',
+      data:$('form').serialize(),
+      complete: function(){
+        $('#formbut').hide();
+        $('#formdone').show();
+              return false;
+      }
+  });
+  return false;
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1200, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
